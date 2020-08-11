04b11097586e1e2dd6e320d1695fb9582263cbfa
BelenCatalan <labdelante@gmail.com>
2020-08-11 19:44:55 +0200

    add controlled form and submit work

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import About from './Sobre-mi';
 import Menu from './Menu';
 import Footer from './Footer';
@@ -7,27 +7,49 @@ import Portfolio from './Portfolio';
 import Contact from './Contacto';
 import { Route, Switch, withRouter, BrowserRouter } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 // import '../stylesheets/styles.css';
 
-const AnimatedSwitch = withRouter(({ location }) => (
-  <TransitionGroup>
-    <CSSTransition key={location.key} classNames="slide" timeout={1000}>
-      <Switch location={location}>
-        <Route path="/" component={About} exact />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/contact" /*component={Contact} */>
-          <Contact handleContactText={handleContactText} />
-        </Route>
-        {/* <Route path="/second" component={Second} />  */}
-      </Switch>
-    </CSSTransition>
-  </TransitionGroup>
-));
-
-const handleContactText = (data) => {
-  console.log(data);
-};
 function App() {
+  // const [formImput, setFormImput] = useState('');
+  // const [emailContact, setEmailContact] = useState('');
+  // const [messageContact, setMessageContact] = useState('');
+
+  /*useState({ contactName: '', contactEmail: '', contactMessage: '' });*/
+
+  const AnimatedSwitch = withRouter(({ location }) => (
+    <TransitionGroup>
+      <CSSTransition key={location.key} classNames="slide" timeout={1000}>
+        <Switch location={location}>
+          <Route path="/" component={About} exact />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" /*component={Contact} */>
+            <Contact /*handleContactText={handleContactText} formImput={formImput}*/ />
+          </Route>
+          {/* <Route path="/second" component={Second} />  */}
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  ));
+
+  // const handleContactText = (data) => {
+  //   console.log(data);
+  //   console.log(data.name);
+  //   if (data.name === 'contactName') {
+  //     setFormImput(data.value);
+  //   }
+  //   if (data.name === 'contacEmail') {
+  //     setEmailContact(data.value);
+  //   } else {
+  //     setMessageContact(data.value);
+  //   }
+
+  //   // setFormImput((prevState) => ({
+  //   //   ...prevState,
+  //   //   [data.name]: data.value,
+  //   // }));
+  // };
+
   return (
     // <div className="">
     <>
@@ -37,7 +59,7 @@ function App() {
         </header>
         <main className="">
           <div className=" main__background App">
-            <AnimatedSwitch handleContactText={handleContactText} />
+            <AnimatedSwitch />
           </div>
         </main>
       </BrowserRouter>
--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,34 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import aboutme from '../images/iconsabout.jpg';
 import '../stylesheets/App.scss';
+import axios from 'axios';
 
-function contact(props) {
+function Contact(props) {
   console.log(props);
-  const handleInputText = (ev) => {
-    props.handleContactText({
-      value: ev.target.value,
-      name: ev.target.name,
+
+  const [formImput, setFormImput] = useState({ contactName: '', contactEmail: '', contactMessage: '' });
+  console.log(formImput);
+
+  // const handleContactText = (ev) => {
+  //   console.log(ev.target.value);
+  //   ev.preventDefault();
+  //   props.handleContactText(
+  //     { value: ev.target.value, name: ev.target.name }
+
+  //     // value: ev.target.value,
+  //     // name: ev.target.name,
+  //   );
+  // };
+
+  const handleTextContact = (ev) => {
+    ev.preventDefault();
+    console.log(ev.target.value);
+    setFormImput({
+      ...formImput,
+      [ev.target.name]: ev.target.value,
     });
+    // const data = {
+    //   value: ev.target.value,
+    //   name: ev.target.name,
   };
 
-  const handlerSubmit = (e) => e.preventDefault();
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null,
+  });
+  const handleServerResponse = (ok, msg) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    });
+    if (ok) {
+      setFormImput({
+        contactName: '',
+        contactEmail: '',
+        contactMessage: '',
+      });
+    }
+  };
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    setServerState({ submitting: true });
+    axios({
+      method: 'POST',
+      url: 'https://formspree.io/mdowjzye',
+      data: formImput,
+    })
+      .then((r) => {
+        handleServerResponse(true, 'Thanks!');
+      })
+      .catch((r) => {
+        handleServerResponse(false, r.response.data.error);
+      });
+  };
 
   return (
     <div className="about__div">
-      <form onSubmit={handlerSubmit} action="" method="GET" id="info-user" className="">
-        <label htmlFor="">Nombre Apellido</label>
-        <input className="" type="" name="name" id="" placeholder="Belén Catalán" onChange={handleInputText} href="" value="" required />
+      <form onSubmit={handleOnSubmit}>
+        <label>Nombre Apellido</label>
+        <input className="" type="text" name="contactName" placeholder="Belén Catalán" onChange={handleTextContact} value={formImput.contactName} />
 
         <label htmlFor="">Email</label>
-        <input className="" type="" name="email" id="" placeholder="email@gmail.com" onChange={handleInputText} href="" value="" required />
+        <input className="" type="text" name="contactEmail" placeholder="email@gmail.com" onChange={handleTextContact} value={formImput.contactEmail} />
 
         <label htmlFor="">
           Mensaje
-          <textarea className="" name="message" onChange={handleInputText}></textarea>
+          <textarea className="" name="contactMessage" onChange={handleTextContact} value={formImput.contactMessage}></textarea>
         </label>
+        <div>
+          <button type="submit" disabled={serverState.submitting}>
+            Enviar
+          </button>
+        </div>
+        {serverState.status && <p className={!serverState.status.ok ? 'errorMsg' : ''}>{serverState.status.msg}</p>}
       </form>
     </div>
   );
 }
 
-export default contact;
+export default Contact;
