1482a80b364ead2640b1e38158704b004e5b9ef7
BelenCatalan <labdelante@gmail.com>
2020-08-13 21:13:25 +0200

    work in grid and icons

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,19 +1,29 @@
 import React from 'react';
 // import aboutme from '../images/iconsabout.jpg';
 import series from '../images/seriecortadados.jpeg';
+import code from '../images/code-fiction.jpeg';
+import cardsjs from '../images/cardsjs.jpeg';
+import cardsreact from '../images/cardsreact.jpeg';
+import randm from '../images/rickandmorty.jpeg';
 import '../stylesheets/App.scss';
 import Project from './Project';
+import { FaHtml5 } from 'react-icons/fa';
+import { FaCss3 } from 'react-icons/fa';
+// import { FaHtml5 } from 'react-icons/fa';
+// import { FaHtml5 } from 'react-icons/fa';
+// import { FaHtml5 } from 'react-icons/fa';
 
 function Portfolio() {
+  const iconQuienes = ['feo', 'casa'];
   return (
     <div className=" portfolio__div portfolio__sectiongrid ">
       {/* <h4 className="about__h4">Belen Catalán</h4> */}
       <ul className="portfolio__wrapper project__ul">
-        <Project text="Buscador de Series" alt="micabeza" image={series} linkto="Ir al proyecto" description="Buscador de series, elige tus series favoritas" />
-        <Project text="segundo" alt="micabeza" />
-        <Project text="tercero" alt="micabeza" />
-        <Project text="cuarto" alt="micabeza" />
-        <Project text="quinto" alt="micabeza" />
+        <Project text="Quiénes somos" alt="team" image={code} icons={iconQuienes} description="Quiénes somos: equipo de desarrollo" />
+        <Project text="Crea tu tarjeta" alt="cardsjs" image={cardsjs} description="Generador de tarjetas digital" />
+        <Project text="Buscador de series" alt="cardsjs" image={series} description="Encuentra tu serie, y añádela a favoritos" />
+        <Project text="Crea tu tarjeta" alt="cardsjs" image={cardsreact} description="Generador de tarjetas digital, código heredado" />
+        <Project text="Rick and Morty" alt="cardsjs" image={randm} description="Buscador de personajes de la serie" />
       </ul>
 
       {/* <img className="about__img" src={aboutme} alt="about me"></img> */}
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,13 @@
 import React, { useState, useRef } from 'react';
 import '../stylesheets/App.scss';
 import { CSSTransition } from 'react-transition-group';
+import { FaHtml5 } from 'react-icons/fa';
+import { FaCss3 } from 'react-icons/fa';
 
 function Project(props) {
+  console.log(props);
+  console.log(props.icons);
+
   const [inHover, setHover] = useState(false);
   // const [inProp, setInProp] = useState(false);
   let changeBack = useRef(false);
@@ -10,6 +15,12 @@ function Project(props) {
   const handleLink = () => {
     console.log('me han clicado');
   };
+
+  // const printIcons = () => {
+  //   props.icons.map((icon) => {
+  //     return <small>{icon}</small>;
+  //   });
+  // };
 
   return (
     <li ref={changeBack} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} className="project__container">
@@ -24,8 +35,9 @@ function Project(props) {
       <div className="project__div">
         <img className="project__img" src={props.image} alt={props.alt}></img>
       </div>
-      <hr className="project__hr"></hr>
-      <small className="project__text--color">{props.linkto}</small>
+      {/* <hr className="project__hr"></hr> */}
+      {/* {printIcons()} */}
+      <small className="project__text--color project__text--small"></small>
       <p className="project__text--color">{props.text}</p>
     </li>
   );
