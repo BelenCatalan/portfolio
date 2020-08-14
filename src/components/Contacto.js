4c089b82694b46e8f1df2d16cbb067fee917a9cb
BelenCatalan <labdelante@gmail.com>
2020-08-14 13:16:34 +0200

    add styles

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 // import aboutme from '../images/iconsabout.jpg';
 import '../stylesheets/App.scss';
 import axios from 'axios';
+import belenmedia from '../images/belenmedia.jpg';
+import belen from '../images/belen.jpg';
 
 function Contact(props) {
   console.log(props);
@@ -53,24 +55,29 @@ function Contact(props) {
 
   return (
     <div className="about__div">
-      <form onSubmit={handleOnSubmit}>
-        <label>Nombre Apellido</label>
-        <input className="" type="text" name="contactName" placeholder="Belén Catalán" onChange={handleTextContact} value={formImput.contactName} />
+      <div className="contact__div">
+        <img className="contact__img" src={belen} alt=""></img>
+      </div>
+      <div className="contact__div--form">
+        <form onSubmit={handleOnSubmit}>
+          <label>Nombre Apellido</label>
+          <input className="" type="text" name="contactName" placeholder="Belén Catalán" onChange={handleTextContact} value={formImput.contactName} />
 
-        <label htmlFor="">Email</label>
-        <input className="" type="text" name="contactEmail" placeholder="email@gmail.com" onChange={handleTextContact} value={formImput.contactEmail} />
+          <label htmlFor="">Email</label>
+          <input className="" type="text" name="contactEmail" placeholder="email@gmail.com" onChange={handleTextContact} value={formImput.contactEmail} />
 
-        <label htmlFor="">
-          Mensaje
-          <textarea className="" name="contactMessage" onChange={handleTextContact} value={formImput.contactMessage}></textarea>
-        </label>
-        <div>
-          <button type="submit" disabled={serverState.submitting}>
-            Enviar
-          </button>
-        </div>
-        {serverState.status && <p className={!serverState.status.ok ? 'errorMsg' : ''}>{serverState.status.msg}</p>}
-      </form>
+          <label htmlFor="">
+            Mensaje
+            <textarea className="" name="contactMessage" onChange={handleTextContact} value={formImput.contactMessage}></textarea>
+          </label>
+          <div>
+            <button type="submit" disabled={serverState.submitting}>
+              Enviar
+            </button>
+          </div>
+          {serverState.status && <p className={!serverState.status.ok ? 'errorMsg' : ''}>{serverState.status.msg}</p>}
+        </form>
+      </div>
     </div>
   );
 }
